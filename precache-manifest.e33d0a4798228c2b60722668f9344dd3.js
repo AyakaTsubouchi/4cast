a87e7bc7654cff720de5b8fec78e7e201678a709
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-02-17 20:54:34 -0800

    Updates

---
+++ precache-manifest.e33d0a4798228c2b60722668f9344dd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33561598bd867da96374c6ad788bd43",
+    "url": "/4cast/index.html"
+  },
+  {
+    "revision": "1b55ff32ba7587c5733d",
+    "url": "/4cast/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3c48551f947e0ee044f5",
+    "url": "/4cast/static/css/main.c53b48e7.chunk.css"
+  },
+  {
+    "revision": "1b55ff32ba7587c5733d",
+    "url": "/4cast/static/js/2.f1d620ca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/4cast/static/js/2.f1d620ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c48551f947e0ee044f5",
+    "url": "/4cast/static/js/main.defb41a1.chunk.js"
+  },
+  {
+    "revision": "85231e17c2ba858aecf4",
+    "url": "/4cast/static/js/runtime-main.6897d0c2.js"
+  }
+]);
