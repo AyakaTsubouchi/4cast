58f9a8303efe5b1c992dd215cfdd72bfc572d6bc
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-02-21 09:57:30 -0800

    add header

--- src/components/weather/Form.js
+++ src/components/weather/Form.js
@@ -1,9 +1,24 @@
 import React from "react";
-
-class Form extends React.Component{
+import { StyleSheet, css } from 'aphrodite';
+const styles = StyleSheet.create({
+    name: {
+        fontFamily: 'Muli',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        fontSize: 14,
+        lineHeight: '20px',
+        textAlign: 'center',
+        letterSpacing: 0.2,
+    //     '@media (max-width: 768px)': {
+    //         display: 'none'
+    // }
+}});
+    
+    
+    class Form extends React.Component{
     render(){
         return(
-            <form onSubmit = {this.props.loadWeather}>
+            <form className={css(styles.name)} onSubmit = {this.props.loadWeather} >
                 <select name="city" placeholder="City ...">
                 <option  value="Vancouver">Vancouver</option>
                 <option  value="Sydney">Sydney</option>
--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { StyleSheet, css } from 'aphrodite';
 
+const styles = StyleSheet.create({
+    name: {
+        fontFamily: 'Muli',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        fontSize: 14,
+        lineHeight: '20px',
+        textAlign: 'center',
+        letterSpacing: 0.2,
+    //     '@media (max-width: 768px)': {
+    //         display: 'none'
+    // }
+}});
 class Weather extends React.Component{
     
     render(){
         const C = Math.round((this.props.temperature)-273.15)+" °C";
         const F = Math.round(((this.props.temperature)-273.15)*9/5+32)+" °F";
         return(
-            <div className="weather-info">
+            <div className="weather-info" className={css(styles.name)}>
             {
                 this.props.city && <p className="weather__key">Location: 
                     <span className="weather__value">  {this.props.city}</span>                    
@@ -23,18 +37,18 @@ class Weather extends React.Component{
                 <p className="weather__key">Temperature(C): 
                     <span className="weather__value"> {C}</span>
                 <br/>
-                Temperature(F): 
-                    <span className="weather__value">{F}</span>
+                {/* Temperature(F): 
+                    <span className="weather__value">{F}</span> */}
                 </p>
          
                 
             }
 
-            {
+            {/* {
                 this.props.humidity && <p className="weather__key">Humidity: 
                     <span className="weather__value">  {this.props.humidity}</span>
                 </p>
-            }
+            } */}
 
             {
                 this.props.description && <p className="weather__key">Conditions:  
